896255cc78d1bb728e90a19c735b96db33a6ed08
kaidon <>
2019-04-23 22:55:18 +0800

    00

--- js/head.js
+++ js/head.js
@@ -1,9 +1,10 @@
-$('#head-menu').on('click', function (e) {
-    if ($(this).hasClass('icon-caidan')) {
-        $(this).removeClass('icon-caidan');
-        $(this).addClass('icon-guanbi');
-    } else {
-        $(this).removeClass('icon-guanbi');
-        $(this).addClass('icon-caidan');
-    }
-})
+$("#head-menu").on("click", function(e) {
+	if ($(this).hasClass("icon-caidan")) {
+		$(this).removeClass("icon-caidan");
+		$(this).addClass("icon-guanbi");
+	} else {
+		$(this).removeClass("icon-guanbi");
+		$(this).addClass("icon-caidan");
+	}
+	$(".submenu").toggleClass("active");
+});
--- js/home.js
+++ js/home.js
@@ -1,21 +1,83 @@
-$().ready(function () {
+var bannerStartX, bannerStartY;
+var bannerCurrentLocation = 1;
 
+function bannerOnTouchStart(e) {
+    bannerStartX = e.originalEvent.changedTouches[0].pageX;
+    bannerStartY = e.originalEvent.changedTouches[0].pageY;
+}
+
+function bannerOnTouchMove(e) {
+	
+	let bannerEndX = e.originalEvent.changedTouches[0].pageX;
+	let bannerEndY = e.originalEvent.changedTouches[0].pageY;
+	let bannerOffsetX = bannerEndX - bannerStartX;
+	let bannerOffsetY = bannerEndY - bannerStartY;
+	if (Math.abs(bannerOffsetY) > 20) {
+        // 滚页操作
+		return;
+    }
+	if (Math.abs(bannerOffsetX) > 20) {
+		if (bannerOffsetX < 0) {
+			bannerSwipeLeft();
+		} else {
+			bannerSwipeRight();
+		}
+	}
+}
+
+function bannerSwipeLeft() {
+	bannerCurrentLocation++;
+	if (bannerCurrentLocation == 4) {
+		bannerCurrentLocation = 0;
+		$(".banner").css("transition", "none");
+		$(".banner").css(
+			"transform",
+			`translateX(-${bannerCurrentLocation * 100}%)`
+		);
+		setTimeout(bannerSwipeLeft, 10);
+	} else {
+		$(".banner").css("transition", "transform 0.5s");
+		$(".banner").css(
+			"transform",
+			`translateX(-${bannerCurrentLocation * 100}%)`
+		);
+	}
+}
+
+function bannerSwipeRight() {
+	bannerCurrentLocation--;
+	if (bannerCurrentLocation == 0) {
+		bannerCurrentLocation = 4;
+		$(".banner").css("transition", "none");
+		$(".banner").css(
+			"transform",
+			`translateX(-${bannerCurrentLocation * 100}%)`
+		);
+		setTimeout(bannerSwipeRight, 10);
+	} else {
+		$(".banner").css("transition", "transform 0.5s");
+		$(".banner").css(
+			"transform",
+			`translateX(-${bannerCurrentLocation * 100}%)`
+		);
+	}
+}
+
+$().ready(function() {
+	$(".banners").on("touchstart", bannerOnTouchStart);
+	$(".banners").on(
+		"touchmove",
+		_.debounce(e => {
+			bannerOnTouchMove(e);
+		}, 50)
+	);
+
+	window.onorientationchange = function() {
+		if (window.orientation == 180 || window.orientation == 0) {
+			// alert('竖屏状态！');
+		}
+		if (window.orientation == 90 || window.orientation == -90) {
+			// alert('横屏状态！');
+		}
+	};
 });
-
-
-
-
-window.onorientationchange = function () {
-    if (window.orientation == 180 || window.orientation == 0) {
-        // alert('竖屏状态！');
-
-
-
-    }
-    if (window.orientation == 90 || window.orientation == -90) {
-        // alert('横屏状态！');
-
-
-
-    }
-};
